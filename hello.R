d798e0211a0c6943908d3560502f532903379c8c
Sakshi <sakshi@Sakshis-MacBook-Air.local>
2026-09-14 13:45:47 +0530

    Update hello.R program

--- hello.R
+++ hello.R
@@ -1,2 +1,13 @@
-name <- "Sakshi"
-print(paste("Hello", name))
+## Write a R program to take input from the user (name and age) and display the values. Also print the version of R installation.
+
+name <- readline(prompt = "Enter your name: ")
+age <- as.numeric(readline(prompt = "Enter your age: "))
+
+# Display the entered values
+print(paste("Name:", name))
+print(paste("Age:", age))
+
+# Display R version
+print("R Version Installed:")
+print(R.version.string)
+
